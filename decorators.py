27e56817cd0f8bdf92c58f3d6e6d265863c14531
vel0cirapt0r <nimazabihi2200@gmail.com>
2024-05-16 13:04:52 +0330

    added colors to Decorators

--- decorators.py
+++ decorators.py
@@ -14,7 +14,17 @@ def measure_time(func):
         start_time = time.time()
         result = func(*args, **kwargs)
         end_time = time.time()
-        logger.info(f"Execution time for {func.__name__}: {end_time - start_time:.6f} seconds")
+        execution_time = end_time - start_time
+        log_message = f"Execution time for {func.__name__}: {execution_time:.6f} seconds"
+
+        # ANSI escape codes for color
+        color_start = "\033[1;32m"  # Green color
+        color_end = "\033[0m"  # Reset color
+
+        # Log message with color
+        colored_log_message = f"{color_start}{log_message}{color_end}"
+
+        logger.info(colored_log_message)
         return result
 
     return wrapper
@@ -28,7 +38,15 @@ def handle_errors(func):
             result = func(*args, **kwargs)
             return result
         except Exception as e:
-            logger.error(f"Error in {func.__name__}: {str(e)}")
+            # ANSI escape codes for color
+            color_start = "\033[1;31m"  # Red color
+            color_end = "\033[0m"  # Reset color
+
+            # Error message with color
+            colored_error_message = f"{color_start}Error in {func.__name__}: {str(e)}{color_end}"
+
+            logger.error(colored_error_message)
             return None
 
     return wrapper
+
