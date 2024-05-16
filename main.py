874e3d32ed9c5f890e9c1025ca219c4503d5cc32
vel0cirapt0r <nimazabihi2200@gmail.com>
2024-05-16 12:59:35 +0330

    dedebuged for type errors that made the algorithm show inf for all distances

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from graph_parser import GraphParser
 from decorators import measure_time
 import filepaths
 
+# from sample_graph_generator import generate_graph
+
 
 class BellmanFord:
     def __init__(self, graph, source):
@@ -14,24 +16,25 @@ class BellmanFord:
 
     @measure_time
     def run(self):
+        # print(type(self.source))
         num_vertices = len(self.graph)
         for _ in range(num_vertices - 1):
             for u in self.graph:
                 for v, weight in self.graph[u].items():
-                    self.relax(u, v, weight)
+                    # print(type(u), type(v))
+                    # print(v, weight, self.distances[u], self.distances[v])
+                    if self.distances[u] + weight < self.distances[v]:
+                        self.distances[v] = self.distances[u] + weight
 
         self.check_negative_cycles()
 
-    def relax(self, u, v, weight):
-        if self.distances[u] + weight < self.distances[v]:
-            self.distances[v] = self.distances[u] + weight
+        return self.distances
 
     def check_negative_cycles(self):
         for u in self.graph:
             for v, weight in self.graph[u].items():
                 if self.distances[u] + weight < self.distances[v]:
                     raise ValueError("Graph contains a negative cycle")
-
 
 
 if __name__ == "__main__":
@@ -41,7 +44,13 @@ if __name__ == "__main__":
     parser = GraphParser()
     parsed_graph = parser.parse(folder_path)
 
-    source = '34'  # Set the source node
+    # total_edges = sum(len(adjacent_vertices) for adjacent_vertices in parsed_graph['distance_edges'].values())
+    # print(len(parsed_graph['distance_edges']), total_edges)
+
+    # graph = generate_graph(20, 95)
+    # print("generated graph: ", graph)
+
+    source = int(input('Enter source for graph: ') or '1')  # Set the source node
     bellman_ford = BellmanFord(parsed_graph['distance_edges'], source)
     bellman_ford.run()
     print("Distances:", bellman_ford.distances)
