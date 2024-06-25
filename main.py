b86a89132d58bf5adfddf007034881cdb6a3f205
vel0cirapt0r <nimazabihi2200@gmail.com>
2024-06-25 18:42:50 +0330

    Add user choice for graph creation or usage in main.py

--- main.py
+++ main.py
@@ -2,9 +2,8 @@ import os
 
 from graph_parser import GraphParser
 from decorators import measure_time
-import filepaths
-
-# from sample_graph_generator import generate_graph
+from filepaths import DATASET_PATH
+from sample_graph_generator import generate_graph
 
 
 class BellmanFord:
@@ -37,20 +36,52 @@ class BellmanFord:
                     raise ValueError("Graph contains a negative cycle")
 
 
+# if __name__ == "__main__":
+#     # dataset_folder_path = input('Enter path to graph files folder: ') or DATASET_PATH
+#     # files_path = input('Enter name of graph files: ') or 'bay'
+#     # folder_path = os.path.join(dataset_folder_path, files_path)
+#     #
+#     # parser = GraphParser()
+#     # parsed_graph = parser.parse(folder_path)
+#     # graph = parsed_graph['distance_edges']
+#     #
+#     # total_edges = sum(len(adjacent_vertices) for adjacent_vertices in graph.values())
+#     # print('number of nodes: ', len(graph), ', number of edges: ', total_edges)
+#     # print(type(graph))
+#
+#     graph = generate_graph(32127, 79483)
+#     # print("generated graph: ", graph)
+#
+#     source = int(input('Enter source for graph: ') or '1')  # Set the source node
+#     bellman_ford = BellmanFord(graph, source)
+#     bellman_ford.run()
+#     print("Distances:", bellman_ford.distances)
+
 if __name__ == "__main__":
-    dataset_folder_path = input('Enter path to graph files folder: ') or filepaths.DATASET_PATH
-    files_path = input('Enter name of graph files: ') or 'bay'
-    folder_path = os.path.join(dataset_folder_path, files_path)
-    parser = GraphParser()
-    parsed_graph = parser.parse(folder_path)
+    print("Bellman-Ford Algorithm Implementation\n")
 
-    total_edges = sum(len(adjacent_vertices) for adjacent_vertices in parsed_graph['distance_edges'].values())
-    print('number of nodes: ', len(parsed_graph['distance_edges']), 'number of edges: ', total_edges)
+    choice = input("Choose an option:\n"
+                   "1. Generate a new graph\n"
+                   "2. Use a prepared graph\n"
+                   "Option: ")
 
-    # graph = generate_graph(20, 95)
-    # print("generated graph: ", graph)
+    if choice == '1':
+        num_nodes = int(input("Enter the number of nodes for the new graph: "))
+        num_edges = int(input("Enter the number of edges for the new graph: "))
+        graph = generate_graph(num_nodes, num_edges)
+    elif choice == '2':
+        dataset_folder_path = input("Enter path to graph files folder: ") or DATASET_PATH
+        files_path = input("Enter name of graph files: ") or 'bay'
+        folder_path = os.path.join(dataset_folder_path, files_path)
 
-    source = int(input('Enter source for graph: ') or '1')  # Set the source node
-    bellman_ford = BellmanFord(parsed_graph['distance_edges'], source)
-    bellman_ford.run()
-    print("Distances:", bellman_ford.distances)
+        parser = GraphParser()
+        parsed_graph = parser.parse(folder_path)
+        graph = parsed_graph['distance_edges']
+    else:
+        print("Invalid choice. Please choose '1' or '2'.")
+        exit()
+
+    source = int(input("Enter source for graph: ") or '1')  # Set the source node
+    bellman_ford = BellmanFord(graph, source)
+    distances = bellman_ford.run()
+    print("Distances:", distances)
